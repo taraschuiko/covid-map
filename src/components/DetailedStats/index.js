76cafdace8bf36e2518f66a44e139cb213fc37a9
Taras Chuiko <taraschuiko@gmail.com>
2022-04-27 15:26:06 +0300

    Added global stats data

--- src/components/DetailedStats/index.js
+++ src/components/DetailedStats/index.js
@@ -3,7 +3,36 @@ import "./DetailedStats.scss";
 import earthIcon from "../../assets/earth-icon.png";
 
 const DetailedStats = () => {
-  return (
+  const [globalStats, setGlobalStats] = React.useState();
+
+  const getGlobalStats = () => {
+    (async () => {
+      let stats = await fetch(
+        "https://disease.sh/v3/covid-19/all?yesterday=true"
+      ).then((r) => r.json());
+
+      stats = await fetch(
+        "https://disease.sh/v3/covid-19/vaccine/coverage?lastdays=2&fullData=false"
+      )
+        .then((r) => r.json())
+        .then((data) => {
+          const values = Object.values(data);
+          return {
+            ...stats,
+            vaccinated: values[0],
+            todayVaccinated: values[0] - values[1],
+          };
+        });
+
+      setGlobalStats(stats);
+    })();
+  };
+
+  React.useEffect(getGlobalStats, []);
+
+  console.log(globalStats);
+
+  return globalStats ? (
     <div className="detailed-stats">
       <div className="detailed-stats-header">
         <img src={earthIcon} alt="Global" />
@@ -13,45 +42,45 @@ const DetailedStats = () => {
         <div>
           <h3>Yesterday</h3>
           <div className="detailed-stats-item">
-            12313123
+            {globalStats.todayCases.toLocaleString()}
             <span>cases</span>
           </div>
           <div className="detailed-stats-item">
-            12313123
+            {globalStats.todayDeaths.toLocaleString()}
             <span>deaths</span>
           </div>
           <div className="detailed-stats-item">
-            12313123
-            <span>vaccinated</span>
+            {globalStats.todayVaccinated.toLocaleString()}
+            <span>vaccine doses</span>
           </div>
           <div className="detailed-stats-item">
-            12313123
+            {globalStats.todayRecovered.toLocaleString()}
             <span>recovered</span>
           </div>
         </div>
         <div>
           <h3>Total</h3>
           <div className="detailed-stats-item">
-            12313123
+            {globalStats.cases.toLocaleString()}
             <span>cases</span>
           </div>
           <div className="detailed-stats-item">
-            12313123
+            {globalStats.deaths.toLocaleString()}
             <span>deaths</span>
           </div>
           <div className="detailed-stats-item">
-            12313123
-            <span>vaccinated</span>
+            {globalStats.vaccinated.toLocaleString()}
+            <span>vaccine doses</span>
           </div>
           <div className="detailed-stats-item">
-            12313123
+            {globalStats.recovered.toLocaleString()}
             <span>recovered</span>
           </div>
         </div>
       </div>
       Updated: 13.04.2022
     </div>
-  );
+  ) : null;
 };
 
 export default DetailedStats;
