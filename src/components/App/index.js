22b2d564cb1e4f731c8423e97a07a39710682692
Taras Chuiko <taraschuiko@gmail.com>
2022-06-20 09:20:25 +0300

    Fixed iOS 100vh bug

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,17 @@
 import "./App.scss";
 import Map from "../Map";
+import Div100vh from "react-div-100vh";
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1>COVID-19 Map</h1>
-      </header>
-      <Map />
-    </div>
+    <Div100vh>
+      <div className="App">
+        <header className="header">
+          <h1>COVID-19 Map</h1>
+        </header>
+        <Map />
+      </div>
+    </Div100vh>
   );
 }
 
